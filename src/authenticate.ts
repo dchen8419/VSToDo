c3b4e924cae0bb42a385866c9aa552b161114e46
David Chen <DavidChen@Davids-iMac.local>
2021-12-16 13:49:26 -0500

    added auth

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,6 +1,30 @@
-import * as vscode from 'vscode'
+import * as vscode from 'vscode';
+import { apiBaseUrl } from './constants';
+import * as polka from 'polka';
 
 
 export const authenticate = () => {
-    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`http://localhost:3002/auth/github`));
+    const app = polka();
+
+    app.get(`/auth/:token`,async (req, res) => {
+        const { token } = req.params;
+        if (!token) {
+            res.end(`<h1>something went wrong</h1>`);
+            return; 
+        }
+
+        console.log(token);
+        res.end(`<h1>auth was successful, you can close now</h1>`);
+    });
+
+    app.listen(5432, (err: Error) => {
+        if (err) {
+            vscode.window.showErrorMessage(err.message);
+        } else {
+            vscode.commands.executeCommand(
+        'vscode.open', 
+        vscode.Uri.parse(`${apiBaseUrl}/auth/github`)
+            );
+        }
+    });
 };
